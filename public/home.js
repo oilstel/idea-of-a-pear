6c52a97f63ca872cf3bea7af777108edd3368c1f
Elliott <elliottcost@gmail.com>
2019-12-24 14:39:12 -0500

    first commit

---
+++ public/home.js
@@ -0,0 +1,196 @@
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
+
+function ready() {
+    var quantityInput = document.getElementById('cart-quantity-input')
+    quantityInput.addEventListener('change', quantityChanged)
+
+    document.getElementById('btn-purchase').addEventListener('click', purchaseClicked)
+
+    updateQuantityDisplay()
+    updateCartTotal()
+    updatePears()
+    resetFields()
+}
+
+function updateQuantityDisplay() {
+    document.getElementById('cart-quantity-display').innerText = document.getElementById('cart-quantity-input').value
+}
+
+var stripeHandler = StripeCheckout.configure({
+    key: stripePublicKey,
+    locale: 'auto',
+    token: function(token) {
+        console.log(token)
+
+        var quantity = document.getElementById('cart-quantity-input').value
+        var fromName = document.getElementById('from-name').value
+        var fromEmail = document.getElementById('from-email').value
+        var recipientsName = document.getElementById('recipients-name').value
+        var recipientsEmail = document.getElementById('recipients-email').value
+
+        fetch('/purchase', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                stripeTokenId: token.id,
+                quantity: quantity,
+                fromName: fromName,
+                fromEmail: fromEmail,
+                recipientsName: recipientsName,
+                recipientsEmail: recipientsEmail
+            })
+        }).then(function(res) {
+            return res.json()
+        }).then(function(data) {
+            alert(data.message)
+            resetFields()
+            document.getElementById('pearview').style.display = 'none'
+            document.body.style.overflowY = 'initial'
+        }).catch(function(error) {
+            console.error(error)
+        })
+    }
+})
+
+function resetFields() {
+    document.getElementById('cart-quantity-input').value = '1'
+    document.getElementById('from-name').value = ''
+    document.getElementById('from-email').value = ''
+    document.getElementById('recipients-name').value = ''
+    document.getElementById('recipients-email').value = ''
+    console.log('yes')
+    updateQuantityDisplay()
+    updateCartTotal()
+    updatePears()
+}
+
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+function purchaseClicked() {
+    if (validateEmail(document.getElementById('recipients-email').value)) {
+        var priceElement = document.getElementsByClassName('cart-total-price')[0]
+        var price = parseFloat(priceElement.innerText.replace('$', '')) * 100
+        var fromEmail = document.getElementById('from-email').value
+        var quantity = document.getElementById('cart-quantity-input').value
+        stripeHandler.open({
+            amount: price,
+            description: `The idea of ${quantity} 🍐`,
+            email: fromEmail
+        })
+        console.log(price)
+    } else {
+        alert('Recipient\'s email is not valid')
+    }
+}
+
+function quantityChanged(event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }
+    updateCartTotal()
+    updatePears()
+    console.log('here')
+}
+
+function updatePears() {
+    var quantity = document.getElementById('cart-quantity-input').value
+
+    if (quantity > 1) {
+        document.getElementById('pear-quantity-text').innerText = quantity + ' pears'
+    } else {
+        document.getElementById('pear-quantity-text').innerText = 'a pear'
+    }
+}
+
+function updateCartTotal() {
+    var total = 0
+
+    var priceElement = document.getElementById('item-price')
+    var quantityElement = document.getElementById('cart-quantity-input')
+    var price = parseFloat(priceElement.innerText.replace('$', ''))
+    var quantity = quantityElement.value
+    total = total + (price * quantity)
+
+    total = Number.parseFloat(Math.round(total * 100) / 100).toFixed(2)
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+}
+
+// Buttons up/down
+document.getElementById('button-down').addEventListener('click', () => {
+    var quantityInput = document.getElementById('cart-quantity-input')
+    if (quantityInput.value >= 2) {
+        quantityInput.stepDown()
+        updateQuantityDisplay()
+        updateCartTotal()
+        updatePears()
+        updatePearview()
+    } else {
+    }
+})
+
+document.getElementById('button-up').addEventListener('click', () => {
+    document.getElementById('cart-quantity-input').stepUp()
+    updateQuantityDisplay()
+    updateCartTotal()
+    updatePears()
+    updatePearview()
+})
+
+// Pearview
+document.getElementById('pearview-open').addEventListener('click', () => {
+    document.getElementById('pearview').style.display = 'flex'
+    document.body.style.overflowY = 'hidden'
+    updatePearview()
+})
+
+document.getElementById('pearview-close').addEventListener('click', () => {
+    document.getElementById('pearview').style.display = 'none'
+    document.body.style.overflowY = 'initial'
+})
+
+updatePearview = () => {
+    var quantity = document.getElementById('cart-quantity-input').value
+    var fromName = document.getElementById('from-name').value
+    var recipientsName = document.getElementById('recipients-name').value
+    var recipientsEmail = document.getElementById('recipients-email').value
+    var pearviewEmailBody = document.getElementById('email')
+
+    pearsArray = []
+
+    for (var i = 0; i < quantity; i++) {
+        pearsArray.push(`🍐`)
+    }
+
+    var pears = pearsArray.join(' ')
+
+    console.log(pears)
+
+    pearviewEmailBody.innerHTML = `
+        <b>from:</b> ${fromName}<br />
+        <b>to:</b> ${recipientsName} <span class="light">&lt;${recipientsEmail}&gt;</span>
+        <br /><br />
+        <div id="pearview-pears">
+            ${pears}
+        </div>
+        <br />
+        Yours truly,<br />
+        ${fromName}
+    `
+}
+
+updatePearview()
+
+
+
+
---
+++ server.js
@@ -0,0 +1,139 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+const express = require('express')
+const app = express()
+const fs = require('fs')
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY_TEST
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY_TEST
+const stripe = require('stripe')(stripeSecretKey)
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(express.static('public'))
+
+
+app.get('/', function(req, res) {
+    fs.readFile('pear.json', function(error, data) {
+        if (error) {
+            res.status(500).end()
+        } else {
+            const itemJson = JSON.parse(data)
+
+            var getPurchases = async () => {
+                try {
+                    const allCharges = await stripe.charges
+                    .list()
+                    .autoPagingToArray({limit: 10000});
+
+                    purchasedArray = []
+                    allCharges.forEach(function(charge) {
+                        var pearQuantity = (Math.floor(charge.amount / itemJson.price))
+                        purchasedArray.push(pearQuantity)
+                    })
+
+                    var purchasedArraySum = arr => purchasedArray.reduce((a,b) => a + b, 0)
+
+                    pearsArray = []
+
+                    console.log(purchasedArraySum())
+
+                    for (i = 0; i < purchasedArraySum(); i++) {
+                        pearsArray.push('🍐')
+                    }
+
+                    pearsHtml = pearsArray.join(' ')
+
+                    res.render('index.ejs', {
+                        stripePublicKey: stripePublicKey,
+                        pear: itemJson,
+                        totalPurchased: pearsHtml
+                    })
+                } catch(err) {
+                    console.log(err)
+                }
+            }
+
+            getPurchases()
+
+        }
+    })
+})
+
+app.post('/purchase', function(req, res) {
+    fs.readFile('pear.json', function(error, data) {
+        if (error) {
+            res.status(500).end()
+        } else {
+            console.log(req.body)
+            
+            const quantity = req.body.quantity
+            const fromName = req.body.fromName
+            const fromEmail = req.body.fromEmail
+            const recipientsName = req.body.recipientsName
+            const recipientsEmail = req.body.recipientsEmail
+            const itemJson = JSON.parse(data)
+            let total = 0
+            total = total + itemJson.price * quantity
+
+            stripe.charges.create({
+                amount: total,
+                source: req.body.stripeTokenId,
+                currency: 'usd'
+            }).then(function() {
+                console.log('Charge Successful')
+                res.json({message: '🍐 sent!'})
+            }).catch(function() {
+                console.log('Charge Fail')
+                res.status(500).end()
+            })
+
+            pearsArray = []
+
+            for(var i = 0; i < quantity; i++) {
+                pearsArray.push(`🍐`)
+            }
+
+            var pears = pearsArray.join(' ')
+            console.log(pears)
+
+            // Send recipient email
+            const emails = [
+                {
+                    to: fromEmail,
+                    from: 'pear@ideaofapear.com',
+                    templateId: 'd-cd1d430ed416465badaad80dbf8fd53a',
+                    dynamic_template_data: {
+                        subject: `Receipt: ${quantity} ideas of a pear sent to ${recipientsName}`,
+                        previewText: pears,
+                        fromName: 'Idea of a Pear',
+                        recipientsName: fromName,
+                        recipientsEmail: fromEmail,
+                        pears: pears,
+                    },
+                },
+                {
+                    to: recipientsEmail,
+                    from: 'pear@ideaofapear.com',
+                    templateId: 'd-cd1d430ed416465badaad80dbf8fd53a',
+                    dynamic_template_data: {
+                        subject: `Received ${quantity} ideas of a pear from ${fromName}`,
+                        previewText: pears,
+                        fromName: fromName,
+                        fromEmail: '<' + fromEmail + '>',
+                        recipientsName: recipientsName,
+                        recipientsEmail: recipientsEmail,
+                        pears: pears,
+                    },
+                }
+            ]
+            sgMail.send(emails);
+        }
+    })
+})
+
+app.listen(3002)
+
